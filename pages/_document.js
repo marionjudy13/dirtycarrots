44f0b2c59dc9a437cf26d03ee8eb0d601db90745
Scott Wambach <scott@scottwamba.ch>
2022-05-22 21:35:08 -0500

    upgraded next, fixed a document prop

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,33 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
       <Html lang="en">
         <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@800&display=swap" rel="stylesheet" />
-        <meta name="description" content="Blog-style archive of all the things I'm learning about development... Mostly for me, but if it can help you, too, then that's great!" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Abhaya+Libre:wght@800&display=swap"
+            rel="stylesheet"
+          />
+          <meta
+            name="description"
+            content="Blog-style archive of all the things I'm learning about development... Mostly for me, but if it can help you, too, then that's great!"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
